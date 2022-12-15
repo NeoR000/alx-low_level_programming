45b066dd934499326e42824f9e6be5bb69dbfef5
NeoR000 <blackoutwear.sa@gmail.com>
2022-12-15 04:48:57 -0800

    task 2

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -16,11 +16,10 @@ int largest_number(int a, int b, int c)
 	{
 		largest = a;
 	}
-
-	 else if (a > b && c > a)
-	 {
-		 largest = c;
-	 }
+	else if (a > b && c > a)
+	{
+		largest = c;
+	}
 	else if (b > c)
 	{
 		largest = b;
@@ -29,6 +28,6 @@ int largest_number(int a, int b, int c)
 	{
 		largest = c;
 	}
-	
+
 	return (largest);
 }
