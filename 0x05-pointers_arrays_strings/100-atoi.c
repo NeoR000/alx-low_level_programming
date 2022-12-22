4bb40f434bf5cc726c8a726588e864ed855b7d85
NeoR000 <blackoutwear.sa@gmail.com>
2022-12-21 18:36:27 -0800

    task 0x05

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,6 +1,5 @@
 #include "main.h"
 
-
 /**
  * _atoi - converts a string to an integer
  * @s: string to be converted
@@ -11,34 +10,36 @@ int _atoi(char *s)
 {
 	int i, d, n, len, f, digit;
 
-	 i = 0;
-	 d = 0;
-	 n = 0;
-	 len = 0;
-	  f = 0;
-	  digit = 0;
+	i = 0;
+	d = 0;
+	n = 0;
+	len = 0;
+	f = 0;
+	digit = 0;
 
-	  while (s[len] != '\0')
-	  {
-		  if (s[i] == '-')
-			  ++d;
+	while (s[len] != '\0')
+		len++;
 
-		  if (s[i] >= '0' && s[i] <= '9')
-		  {
-			  digit = s[i] - '0';
-			  if (d % 2)
-				  digit = -digit;
-			  n = n * 10 + digit;
-			  f = 1;
-			  if (s[i + 1] < '0' || s[i + 1] > '9')
-				  break;
-			  f = 0;
-		  }
-		  i++;
-	  }
-	  if (f == 0)
-		  return (0);
+	while (i < len && f == 0)
+	{
+		if (s[i] == '-')
+			++d;
 
-	  return (n);
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			digit = s[i] - '0';
+			if (d % 2)
+				digit = -digit;
+			n = n * 10 + digit;
+			f = 1;
+			if (s[i + 1] < '0' || s[i + 1] > '9')
+				f = 0;
+		}
+		i++;
+	}
+
+	if (f == 0)
+		return (0);
+
+	return (n);
 }
-
