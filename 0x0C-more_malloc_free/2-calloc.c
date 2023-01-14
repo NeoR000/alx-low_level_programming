40dfd54d736bcc77df71e50101d0d5f6ab7d6f72
NeoR000 <blackoutwear.sa@gmail.com>
2023-01-13 18:23:56 -0800

    task 0x0c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -13,8 +13,8 @@ char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
 
-
 	for (i = 0; i < n; i++)
+
 	{
 		s[i] = b;
 	}
@@ -30,7 +30,6 @@ char *_memset(char *s, char b, unsigned int n)
 	void *_calloc(unsigned int nmemb, unsigned int size)
 	{
 		char *ptr;
-
 
 		if (nmemb == 0 || size == 0)
 
