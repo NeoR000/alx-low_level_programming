18d294792742333e5051826f4cd6f6e0f04508d9
NeoR000 <blackoutwear.sa@gmail.com>
2023-02-01 17:38:34 -0800

    task 0x13

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -2,7 +2,7 @@
 
 
 /**
-  free_listint2 - frees a linked list
+ * free_listint2 - frees a linked list
  * @head: pointer to the listint_t list to be freed
  */
 void free_listint2(listint_t **head)
